aa6b0b5f4c481d8a563583177e014d7e862f9cf1
bokycodes <114671954+bokycodes@users.noreply.github.com>
2022-12-29 20:34:55 +0100

    Added a few new buttons and design

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mouseoverDivs();
 
 function createDiv(gridSize){
     const div = document.createElement("div");
-    div.style.cssText = "border: 1px solid black;";
+    div.style.cssText = "border: 1px solid black; background-color: white;";
     container.appendChild(div);
 }
 
@@ -22,6 +22,9 @@ function mouseoverDivs(){
         });
     }
 }
+
+
+// MAKE NEW GRID BUTTON
 
 const btnChangeGrid = document.querySelector(".btnChangeGrid");
 
@@ -57,4 +60,15 @@ btnChangeGrid.addEventListener("click", () => {
     }
 
     mouseoverDivs();
+});
+
+
+// DISABLE GRID LINES BTN
+const btnDisableGridLines = document.querySelector(".btnDisableGridLines");
+btnDisableGridLines.addEventListener("click", () => {
+    const divList = document.querySelectorAll("div");
+    for(let i = 0; i < divList.length; i++){
+        divList[i].style.border = "none";
+    }
+    
 });
